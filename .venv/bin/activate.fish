ca128aac8576ba830a08545799a66cd99747da59
Gajendra Babu <mayajal@users.noreply.github.com>
2025-07-29 15:12:32 +0530

    virtual env rebuilt

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/user/studio/.venv
+set -gx VIRTUAL_ENV /Users/gajendrababubaktavachalam/WebApps/statviz/stats/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
