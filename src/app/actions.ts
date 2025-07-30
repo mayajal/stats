beecbb3c8dcf5ee85febf9f8eb695488c91e9857
Gajendra Babu <bgbabu@gmail.com>
2025-07-30 05:20:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/app/actions.ts
@@ -0,0 +1,13 @@
+'use server';
+
+import {
+  getStatisticalGuidance as getStatisticalGuidanceFlow,
+  type StatisticalGuidanceInput,
+  type StatisticalGuidanceOutput,
+} from '@/ai/flows/statistical-guide-flow';
+
+export async function getStatisticalGuidance(
+  input: StatisticalGuidanceInput
+): Promise<StatisticalGuidanceOutput> {
+  return await getStatisticalGuidanceFlow(input);
+}
