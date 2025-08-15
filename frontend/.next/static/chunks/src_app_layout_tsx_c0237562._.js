4a1a6e8180eb1ea74a5cac1d207eba08328be29a
Gajendra Babu <mayajal@users.noreply.github.com>
2025-08-15 18:00:44 +0530

    latest commit

--- frontend/.next/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__873b54ef._.css",
-  "static/chunks/node_modules_next_dist_83f8db82._.js"
-],
-    source: "dynamic"
-});
